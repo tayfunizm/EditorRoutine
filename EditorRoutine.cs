609c8c6f39296d9ea890aed0e7e075fb7dcf8f6b
Tayfun YILDIRIM <34584422+tayfunizm@users.noreply.github.com>
2023-04-05 01:44:08 +0300

    Update EditorRoutine.cs

--- EditorRoutine.cs
+++ EditorRoutine.cs
@@ -1,69 +1,67 @@
 using System;
 using UnityEditor;
 
-namespace Nullframes.IR.IE.Utils {
-    public sealed class IERoutine {
-        private readonly Func<bool> waitUntil;
-        private readonly float delay;
+public sealed class IERoutine {
+    private readonly Func<bool> waitUntil;
+    private readonly float delay;
         
-        private double nextTime;
-        private event Action call;
+    private double nextTime;
+    private event Action call;
 
-        public IERoutine(Action call, float delay, Func<bool> waitUntil) {
-            this.call += call;
-            this.delay = delay;
-            this.waitUntil = waitUntil;
-            this.nextTime = EditorApplication.timeSinceStartup + delay;
+    public IERoutine(Action call, float delay, Func<bool> waitUntil) {
+        this.call += call;
+        this.delay = delay;
+        this.waitUntil = waitUntil;
+        this.nextTime = EditorApplication.timeSinceStartup + delay;
 
-            this.call += () => { EditorApplication.update -= Update; };
+        this.call += () => { EditorApplication.update -= Update; };
+    }
+
+    public void Update() {
+        if (!(EditorApplication.timeSinceStartup > nextTime)) return;
+        if (waitUntil == null) {
+            Execute();
+            nextTime = EditorApplication.timeSinceStartup + delay;
+            return;
         }
 
-        public void Update() {
-            if (!(EditorApplication.timeSinceStartup > nextTime)) return;
-            if (waitUntil == null) {
-                Execute();
-                nextTime = EditorApplication.timeSinceStartup + delay;
-                return;
-            }
-
-            if (waitUntil.Invoke()) {
-                Execute();
-            }
-        }
-
-        private void Execute() {
-            call?.Invoke();
-        }
-
-        public void Dispose() {
-            EditorApplication.update -= Update;
+        if (waitUntil.Invoke()) {
+            Execute();
         }
     }
 
-    public static class EditorRoutine {
-        public static IERoutine StartRoutine(float delay, Action call) {
-            IERoutine routine = new IERoutine(call, delay, null);
+    private void Execute() {
+        call?.Invoke();
+    }
 
-            EditorApplication.update += routine.Update;
-            return routine;
-        }
+    public void Dispose() {
+        EditorApplication.update -= Update;
+    }
+}
 
-        public static IERoutine StartRoutine(Func<bool> waitUntil, Action call) {
-            IERoutine routine = new IERoutine(call, 0f, waitUntil);
+public static class EditorRoutine {
+    public static IERoutine StartRoutine(float delay, Action call) {
+        IERoutine routine = new IERoutine(call, delay, null);
 
-            EditorApplication.update += routine.Update;
-            return routine;
-        }
+        EditorApplication.update += routine.Update;
+        return routine;
+    }
+
+    public static IERoutine StartRoutine(Func<bool> waitUntil, Action call) {
+        IERoutine routine = new IERoutine(call, 0f, waitUntil);
+
+        EditorApplication.update += routine.Update;
+        return routine;
+    }
         
-        public static IERoutine StartRoutine(float delay, Func<bool> waitUntil, Action call) {
-            IERoutine routine = new IERoutine(call, delay, waitUntil);
+    public static IERoutine StartRoutine(float delay, Func<bool> waitUntil, Action call) {
+        IERoutine routine = new IERoutine(call, delay, waitUntil);
 
-            EditorApplication.update += routine.Update;
-            return routine;
-        }
+        EditorApplication.update += routine.Update;
+        return routine;
+    }
 
-        public static void StopRoutine(IERoutine routine) {
-            routine.Dispose();
-        }
+    public static void StopRoutine(IERoutine routine) {
+        routine.Dispose();
     }
 }
